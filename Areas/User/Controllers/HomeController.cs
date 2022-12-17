839655e090c79c364648a1a0de33fe05f0954b37
Trinh Nguyễn Thị Mai <19522405@ms.uit.edu.vn>
2022-12-17 10:32:23 +0700

    Trinhadd

--- Areas/User/Controllers/HomeController.cs
+++ Areas/User/Controllers/HomeController.cs
@@ -1,15 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QLBH.Models;
 //using QLBH.Models;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace QLBH.Areas.User.Controllers
 {
     public class HomeController : Controller
     {
         [Area("user")]
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            HomeModel homeModel = new HomeModel();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7032/api/");
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Top Albums
+                HttpResponseMessage getDataTopalbums = await client.GetAsync("Album/TopAlbum/5");
+
+                if (getDataTopalbums.IsSuccessStatusCode)
+                {
+                    string results = getDataTopalbums.Content.ReadAsStringAsync().Result;
+                    homeModel.TopAlbums = JsonConvert.DeserializeObject<List<AlbumLink>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling web API");
+                }
+
+                //Top Albums Nổi bật
+                HttpResponseMessage getDataAlbumNB = await client.GetAsync("Album/GetTGPhatHanh/5");
+
+                if (getDataAlbumNB.IsSuccessStatusCode)
+                {
+                    string results = getDataAlbumNB.Content.ReadAsStringAsync().Result;
+                    homeModel.AlbumsNoiBat = JsonConvert.DeserializeObject<List<AlbumLink>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling web API");
+                }
+
+                //Top BaiHat
+                HttpResponseMessage getDataTopBaiHat = await client.GetAsync("BaiHat/GetTop/9");
+
+                if (getDataTopBaiHat.IsSuccessStatusCode)
+                {
+                    string results = getDataTopBaiHat.Content.ReadAsStringAsync().Result;
+                    homeModel.TopBaiHats = JsonConvert.DeserializeObject<List<BaiHatLink>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling web API");
+                }
+                ViewData.Model = homeModel;
+
+                return View();
+            }
         }
         [Area("user")]
         public ActionResult About()
