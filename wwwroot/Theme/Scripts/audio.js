8b83cbc1f5379ce8ee8403a7fca07b0135c36c67
LuuPhuc01 <phucluu7271@gmail.com>
2022-12-17 17:28:57 +0700

    Phuc commit

--- wwwroot/Theme/Scripts/audio.js
+++ wwwroot/Theme/Scripts/audio.js
@@ -52,20 +52,20 @@ const musics = new Array(
         image:
             "https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     },
-/*    {
+    {
         id: 2,
         title: "Xuân Thì",
         file: "../../theme/media/xuanthi.mp3",
         image:
             "https://images.unsplash.com/photo-1614624532983-4ce03382d63d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
-    },*/
-/*    {
+    },
+    {
         id: 3,
         title: "Anh Nhớ Ra",
         file: "../../theme/media/anhnhora.mp3",
         image:
             "https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },*/
+    },
 )
 
 
