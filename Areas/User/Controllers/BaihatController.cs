b429a398f327d81e4f033b1c5131bb8fadea0457
TNMyTien <92216495+TNMyTien@users.noreply.github.com>
2022-12-18 15:29:55 +0700

    Them BaihatController

---
+++ Areas/User/Controllers/BaihatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLBH.Areas.User.Controllers
+{
+    public class BaihatController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
