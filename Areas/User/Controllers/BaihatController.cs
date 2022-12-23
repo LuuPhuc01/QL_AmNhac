ae02e5f9035fcdd0590bb810a5fc7870b1cf5885
Trinh Nguyễn Thị Mai <19522405@ms.uit.edu.vn>
2022-12-23 14:03:36 +0700

    add new

--- Areas/User/Controllers/BaihatController.cs
+++ Areas/User/Controllers/BaihatController.cs
@@ -11,26 +11,79 @@ namespace QLBH.Areas.User.Controllers
         [Area("user")]
         public async Task<IActionResult> Index(int id)
         {
-            NgheSiLink ns = new NgheSiLink();
+            HomeModel pm = new HomeModel();
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:7032/api/");
 
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage getData = await client.GetAsync($"NgheSi/{id}");
+                //bai hat 
+                HttpResponseMessage getData = await client.GetAsync($"BaiHat/{id}");
 
                 if (getData.IsSuccessStatusCode)
                 {
                     string results = getData.Content.ReadAsStringAsync().Result;
-                    ns = JsonConvert.DeserializeObject<NgheSiLink>(results);
+                    pm.BaiHat = JsonConvert.DeserializeObject<BaiHatLink>(results);
+                    
+                    //top bai hat nghe si
+                    HttpResponseMessage getDatatopbh = await client.GetAsync($"NgheSi/TopBaiHatNgheSi/{id}");
+
+                    if (getDatatopbh.IsSuccessStatusCode)
+                    {
+                        string resultsbh = getDatatopbh.Content.ReadAsStringAsync().Result;
+                        pm.TopBaiHats = JsonConvert.DeserializeObject<List<BaiHatLink>>(resultsbh);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Error calling web API");
                 }
-                ViewData.Model = ns;
+
+                // bai hat yeu thich
+                var Usertoken = HttpContext.Session.GetString("tokenUser");
+                //Console.WriteLine(userToken);
+                if (Usertoken != null)
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Usertoken);
+
+                    HttpResponseMessage getDatabhyt = await client.GetAsync("my/BaihatUser/GetAll");
+
+
+                    if (getDatabhyt.IsSuccessStatusCode)
+                    {
+                        string results = getDatabhyt.Content.ReadAsStringAsync().Result;
+                        //Console.WriteLine(results);
+                        pm.BaiHatYeuThichs = JsonConvert.DeserializeObject<List<BaiHat>>(results);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
+                }
+                // playlist user
+                if (Usertoken != null)
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Usertoken);
+
+                    HttpResponseMessage getDataplaylist = await client.GetAsync("my/Playlist/GetAll");
+
+
+                    if (getDataplaylist.IsSuccessStatusCode)
+                    {
+                        string results = getDataplaylist.Content.ReadAsStringAsync().Result;
+                        pm.Playlists = JsonConvert.DeserializeObject<List<PlaylistLink>>(results);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
+                }
+                ViewData.Model = pm;
                 return View();
             }
         }
