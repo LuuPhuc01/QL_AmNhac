5df88e1521bc465f215adbb976230d08cfb56d5b
Trinh Nguyễn Thị Mai <19522405@ms.uit.edu.vn>
2022-12-25 09:52:42 +0700

    admin

--- Areas/Admin/Controllers/QL_AlbumController.cs
+++ Areas/Admin/Controllers/QL_AlbumController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using QLBH.Models;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace QLBH.Areas.Admin.Controllers
 {
@@ -37,6 +38,73 @@ namespace QLBH.Areas.Admin.Controllers
                 ViewData.Model = qg;
                 return View();
             }
+        }
+        [Area("admin")]
+
+        public async Task<IActionResult> ThemAlbum()
+        {
+            if (HttpContext.Session.GetString("token") == null)
+            {
+                
+
+                return RedirectToAction("dangnhap", "ql", new { area = "Admin" });
+            }
+            else
+            {
+                IList<NgheSi> qg = new List<NgheSi>();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7032/api/");
+
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage getData = await client.GetAsync("NgheSi/GetAll");
+
+                    if (getData.IsSuccessStatusCode)
+                    {
+                        string results = getData.Content.ReadAsStringAsync().Result;
+                        qg = JsonConvert.DeserializeObject<List<NgheSi>>(results);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
+                    ViewData.Model = qg;
+
+                    return View();
+                }
+            }
+        }
+        [Area("admin")]
+
+        [HttpPost]
+        public async Task<IActionResult> ThemAlbum(Album album)
+        {
+            if (HttpContext.Session.GetString("token") == null)
+            {
+                return RedirectToAction("dangnhap", "ql", new { area = "Admin" });
+            }
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7032/api/");
+
+                var response = client.PostAsJsonAsync<Album>("album", album);
+                response.Wait();
+
+                var test = response.Result;
+                //Console.WriteLine(response.Result);
+                if (test.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+
+            }
+            return View("~/admin/QL_NgheSi/ThemNgheSi");
         }
     }
 }
