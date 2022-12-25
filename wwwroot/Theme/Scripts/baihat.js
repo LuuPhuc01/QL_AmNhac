8374f8e22464c51201b24fe186bc8bb15eb7cc1f
TNMyTien <92216495+TNMyTien@users.noreply.github.com>
2022-12-25 19:28:39 +0700

    them file: xu ly loi bai hat

---
+++ wwwroot/Theme/Scripts/baihat.js
@@ -0,0 +1,20 @@
+﻿const loiBH = document.getElementById("loiBH");
+
+function convertBR() {
+    var lyricInit = loiBH.innerHTML; 
+    console.log("input:"+lyricInit);
+    var output = "";
+
+    for (var i = 0; i < lyricInit.length; i++) {
+        if ((lyricInit.charCodeAt(i) == 13) && (lyricInit.charCodeAt(i + 1) == 10)) {
+            output += "<BR>";
+            i++;
+        } else {
+            output += lyricInit.charAt(i);
+        }
+    }
+    console.log('output: ' + output);
+    loiBH.innerHTML = output;
+}
+
+convertBR();
