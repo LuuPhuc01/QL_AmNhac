7f0b0b0f990c9f8dc206ecbb833f06719f31e373
Trinh Nguyễn Thị Mai <19522405@ms.uit.edu.vn>
2022-12-26 21:54:30 +0700

    download song

---
+++ wwwroot/Theme/Scripts/downloadSingle.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+
+	const downloadBtn = document.querySelector(".btn-download_single");
+
+	downloadBtn.addEventListener("click", () => {
+		const fileInput = downloadBtn.parentElement.children[1];
+		const atag = downloadBtn.parentElement.children[0];
+
+			fetchFile(fileInput.value);
+
+			function fetchFile(url) {
+				fetch(url).then(res => res.blob()).then(file => {
+					let tempUrl = URL.createObjectURL(file);
+
+					atag.href = tempUrl;
+					atag.click();
+					atag.remove();
+				});
+			}
+		})
+});
